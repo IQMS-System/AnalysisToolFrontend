3559126a9d5558ca2e29fd3356dbc981cdea264f
nugrahaa878 <nugrahaa878@gmail.com>
2024-07-07 16:44:14 +0700

    feat: create profile page

--- src/routes/ProfilePage/index.tsx
+++ src/routes/ProfilePage/index.tsx
@@ -1,9 +1,35 @@
+import { Descriptions, DescriptionsProps, Flex, Typography } from "antd";
 import BaseLayout from "../../components/BaseLayout";
+
+const { Title } = Typography;
+
+const items: DescriptionsProps["items"] = [
+  {
+    key: "1",
+    label: "Name",
+    children: "Budi Santoso",
+  },
+  {
+    key: "2",
+    label: "Username",
+    children: "budi_santoso",
+  },
+  {
+    key: "3",
+    label: "Level",
+    children: "Admin",
+  },
+];
 
 const ProfilePage = () => {
   return (
     <BaseLayout breadCrumb={["Home", "Profile"]}>
-      <div>Ini profile page</div>
+      <Flex vertical gap={10}>
+        <Flex justify="space-between">
+          <Title level={3}>User Profile</Title>
+        </Flex>
+        <Descriptions items={items} column={1} />
+      </Flex>
     </BaseLayout>
   );
 };
