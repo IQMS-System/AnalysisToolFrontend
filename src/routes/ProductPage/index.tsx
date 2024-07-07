fa1950f2b9d15caa268aab1752b1d6d3e715c362
nugrahaa878 <nugrahaa878@gmail.com>
2024-07-07 16:57:26 +0700

    feat: create list product

--- src/routes/ProductPage/index.tsx
+++ src/routes/ProductPage/index.tsx
@@ -1,18 +1,38 @@
-import { Button, Flex, Typography } from "antd";
+import { Button, Card, Flex, Typography } from "antd";
 import BaseLayout from "../../components/BaseLayout";
 import { PlusOutlined } from "@ant-design/icons";
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
+
+const dummyData = [
+  "Product A",
+  "Product B",
+  "Product C",
+  "Product D",
+  "Product E",
+  "Product F",
+  "Product G",
+  "Product H",
+  "Product I",
+];
 
 const ProductPage = () => {
   return (
     <BaseLayout breadCrumb={["Home", "Products"]}>
-      <Flex vertical gap={10}>
+      <Flex vertical gap={50}>
         <Flex justify="space-between">
           <Title level={3}>Product Management</Title>
           <Button type="primary" icon={<PlusOutlined />}>
             Create a Product Hierarchy Template
           </Button>
+        </Flex>
+
+        <Flex wrap justify="center" gap={50}>
+          {dummyData.map((card) => (
+            <Card style={{ width: 300, textAlign: "center" }} key={card}>
+              <Text strong>{card}</Text>
+            </Card>
+          ))}
         </Flex>
       </Flex>
     </BaseLayout>
