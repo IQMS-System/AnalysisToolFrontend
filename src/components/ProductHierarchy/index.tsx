9a9691857b6ffb4a0cf76a60acf43f6db27aa476
nugrahaa878 <nugrahaa878@gmail.com>
2024-07-08 08:31:56 +0700

    feat: add upload page

---
+++ src/components/ProductHierarchy/index.tsx
@@ -0,0 +1,87 @@
+import { Button, Flex, Select, Typography } from "antd";
+import { Content } from "antd/es/layout/layout";
+
+const { Text } = Typography;
+const { Option } = Select;
+
+const ProductHierarchy = () => {
+  return (
+    <Flex justify="space-between" vertical>
+      <Content style={{ padding: "20px" }}>
+        <Flex gap={20}>
+          <Flex vertical gap={10}>
+            <Text className="pl-1">Level 1</Text>
+            <Select
+              defaultValue="option1"
+              style={{ width: 200 }}
+              onChange={() => {}}
+              title="asdasd"
+            >
+              <Option value="option1">Option 1</Option>
+              <Option value="option2">Option 2</Option>
+              <Option value="option3">Option 3</Option>
+            </Select>
+          </Flex>
+          <Flex vertical gap={10}>
+            <Text className="pl-1">Level 2</Text>
+            <Select
+              defaultValue="option1"
+              style={{ width: 200 }}
+              onChange={() => {}}
+              title="asdasd"
+            >
+              <Option value="option1">Option 1</Option>
+              <Option value="option2">Option 2</Option>
+              <Option value="option3">Option 3</Option>
+            </Select>
+          </Flex>
+          <Flex vertical gap={10}>
+            <Text className="pl-1">Level 3</Text>
+            <Select
+              defaultValue="option1"
+              style={{ width: 200 }}
+              onChange={() => {}}
+              title="asdasd"
+            >
+              <Option value="option1">Option 1</Option>
+              <Option value="option2">Option 2</Option>
+              <Option value="option3">Option 3</Option>
+            </Select>
+          </Flex>
+          <Flex vertical gap={10}>
+            <Text className="pl-1">Level 4</Text>
+            <Select
+              defaultValue="option1"
+              style={{ width: 200 }}
+              onChange={() => {}}
+              title="asdasd"
+            >
+              <Option value="option1">Option 1</Option>
+              <Option value="option2">Option 2</Option>
+              <Option value="option3">Option 3</Option>
+            </Select>
+          </Flex>
+          <Flex vertical gap={10}>
+            <Text className="pl-1">Level 5</Text>
+            <Select
+              defaultValue="option1"
+              style={{ width: 200 }}
+              onChange={() => {}}
+              title="asdasd"
+            >
+              <Option value="option1">Option 1</Option>
+              <Option value="option2">Option 2</Option>
+              <Option value="option3">Option 3</Option>
+            </Select>
+          </Flex>
+        </Flex>
+
+        <Button type="primary" className="mt-10">
+          Submit
+        </Button>
+      </Content>
+    </Flex>
+  );
+};
+
+export default ProductHierarchy;
--- src/routes/UploadPage/index.tsx
+++ src/routes/UploadPage/index.tsx
@@ -1,9 +1,25 @@
+import { Button, Divider, Flex, Typography } from "antd";
 import BaseLayout from "../../components/BaseLayout";
+import ProductHierarchy from "../../components/ProductHierarchy";
+
+const { Title } = Typography;
 
 const UploadPage = () => {
   return (
     <BaseLayout breadCrumb={["Home", "Upload"]}>
-      <div>Ini upload page</div>
+      <Flex vertical>
+        <Title level={3}>Upload Part</Title>
+
+        <Flex className="mt-5 mb-2" gap={10}>
+          <Button type="primary" className="bg-green-700">
+            Checksheet
+          </Button>
+          <Button>Measurement Files</Button>
+        </Flex>
+        <Divider />
+
+        <ProductHierarchy />
+      </Flex>
     </BaseLayout>
   );
 };
