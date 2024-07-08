86fd163e1f537e9e8de1eaa56fc703e2c86bf53d
nugrahaa878 <nugrahaa878@gmail.com>
2024-07-08 09:07:07 +0700

    feat: add page report

--- src/routes/QualityControlPage/index.tsx
+++ src/routes/QualityControlPage/index.tsx
@@ -1,9 +1,19 @@
+import { Flex, Typography } from "antd";
 import BaseLayout from "../../components/BaseLayout";
+import ProductHierarchy from "../../components/ProductHierarchy";
+
+const { Title } = Typography;
 
 const QualityControlPage = () => {
   return (
     <BaseLayout breadCrumb={["Home", "Quality Control"]}>
-      <div>Ini quality page control</div>
+      <Flex vertical gap={20}>
+        <Flex justify="space-between">
+          <Title level={3}>Approval Configuration</Title>
+        </Flex>
+
+        <ProductHierarchy />
+      </Flex>
     </BaseLayout>
   );
 };
