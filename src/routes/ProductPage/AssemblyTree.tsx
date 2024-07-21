ac014a966937f59f26cae77f0c037893e17bcf27
nugrahaa878 <nugrahaa878@gmail.com>
2024-07-21 09:35:26 +0700

    feat: add button edit spesification in create product

--- src/routes/ProductPage/AssemblyTree.tsx
+++ src/routes/ProductPage/AssemblyTree.tsx
@@ -3,27 +3,41 @@ import { Card, Row, Col, Form, Input, Button } from "antd";
 
 const { Item: FormItem } = Form;
 
+interface Specification {
+  key: string;
+  featureName: string;
+  nominalDimension: string;
+}
+
 interface SubPart {
   id: string;
   name: string;
+  specifications: Specification[];
+  showSpecifications: boolean;
 }
 
 interface Part {
   id: string;
   name: string;
   subParts: SubPart[];
+  specifications: Specification[];
+  showSpecifications: boolean;
 }
 
 interface SubAssembly {
   id: string;
   name: string;
   parts: Part[];
+  specifications: Specification[];
+  showSpecifications: boolean;
 }
 
 interface Assembly {
   id: string;
   name: string;
   subAssemblies: SubAssembly[];
+  specifications: Specification[];
+  showSpecifications: boolean;
 }
 
 const AssemblyForm: React.FC = () => {
@@ -31,6 +45,8 @@ const AssemblyForm: React.FC = () => {
     id: "1",
     name: "",
     subAssemblies: [],
+    specifications: [],
+    showSpecifications: false,
   });
 
   const addSubAssembly = () => {
@@ -38,7 +54,10 @@ const AssemblyForm: React.FC = () => {
       id: (assembly.subAssemblies.length + 1).toString(),
       name: "",
       parts: [],
+      specifications: [],
+      showSpecifications: false,
     };
+
     setAssembly((prevAssembly) => ({
       ...prevAssembly,
       subAssemblies: [...prevAssembly.subAssemblies, newSubAssembly],
@@ -67,6 +86,8 @@ const AssemblyForm: React.FC = () => {
                   id: (subAssembly.parts.length + 1).toString(),
                   name: "",
                   subParts: [],
+                  specifications: [],
+                  showSpecifications: false,
                 },
               ],
             }
@@ -111,6 +132,8 @@ const AssemblyForm: React.FC = () => {
                         {
                           id: (part.subParts.length + 1).toString(),
                           name: "",
+                          specifications: [],
+                          showSpecifications: false,
                         },
                       ],
                     }
@@ -152,6 +175,136 @@ const AssemblyForm: React.FC = () => {
     }));
   };
 
+  const toggleSpecifications = (
+    id: string,
+    type: "assembly" | "subAssembly" | "part" | "subPart"
+  ) => {
+    switch (type) {
+      case "assembly":
+        setAssembly((prevAssembly) => ({
+          ...prevAssembly,
+          showSpecifications: !prevAssembly.showSpecifications,
+        }));
+        break;
+      case "subAssembly":
+        setAssembly((prevAssembly) => ({
+          ...prevAssembly,
+          subAssemblies: prevAssembly.subAssemblies.map((subAssembly) =>
+            subAssembly.id === id
+              ? {
+                  ...subAssembly,
+                  showSpecifications: !subAssembly.showSpecifications,
+                }
+              : subAssembly
+          ),
+        }));
+        break;
+      case "part":
+        setAssembly((prevAssembly) => ({
+          ...prevAssembly,
+          subAssemblies: prevAssembly.subAssemblies.map((subAssembly) => ({
+            ...subAssembly,
+            parts: subAssembly.parts.map((part) =>
+              part.id === id
+                ? { ...part, showSpecifications: !part.showSpecifications }
+                : part
+            ),
+          })),
+        }));
+        break;
+      case "subPart":
+        setAssembly((prevAssembly) => ({
+          ...prevAssembly,
+          subAssemblies: prevAssembly.subAssemblies.map((subAssembly) => ({
+            ...subAssembly,
+            parts: subAssembly.parts.map((part) => ({
+              ...part,
+              subParts: part.subParts.map((subPart) =>
+                subPart.id === id
+                  ? {
+                      ...subPart,
+                      showSpecifications: !subPart.showSpecifications,
+                    }
+                  : subPart
+              ),
+            })),
+          })),
+        }));
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleSpecChange = (
+    index: number,
+    field: string,
+    value: string,
+    id: string,
+    type: "assembly" | "subAssembly" | "part" | "subPart"
+  ) => {
+    const updateSpecs = (specs: Specification[]) =>
+      specs.map((spec, idx) =>
+        idx === index ? { ...spec, [field]: value } : spec
+      );
+
+    switch (type) {
+      case "assembly":
+        setAssembly((prevAssembly) => ({
+          ...prevAssembly,
+          specifications: updateSpecs(prevAssembly.specifications),
+        }));
+        break;
+      case "subAssembly":
+        setAssembly((prevAssembly) => ({
+          ...prevAssembly,
+          subAssemblies: prevAssembly.subAssemblies.map((subAssembly) =>
+            subAssembly.id === id
+              ? {
+                  ...subAssembly,
+                  specifications: updateSpecs(subAssembly.specifications),
+                }
+              : subAssembly
+          ),
+        }));
+        break;
+      case "part":
+        setAssembly((prevAssembly) => ({
+          ...prevAssembly,
+          subAssemblies: prevAssembly.subAssemblies.map((subAssembly) => ({
+            ...subAssembly,
+            parts: subAssembly.parts.map((part) =>
+              part.id === id
+                ? { ...part, specifications: updateSpecs(part.specifications) }
+                : part
+            ),
+          })),
+        }));
+        break;
+      case "subPart":
+        setAssembly((prevAssembly) => ({
+          ...prevAssembly,
+          subAssemblies: prevAssembly.subAssemblies.map((subAssembly) => ({
+            ...subAssembly,
+            parts: subAssembly.parts.map((part) => ({
+              ...part,
+              subParts: part.subParts.map((subPart) =>
+                subPart.id === id
+                  ? {
+                      ...subPart,
+                      specifications: updateSpecs(subPart.specifications),
+                    }
+                  : subPart
+              ),
+            })),
+          })),
+        }));
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div>
       <Card title="Assembly">
@@ -165,8 +318,13 @@ const AssemblyForm: React.FC = () => {
             />
           </FormItem>
           <FormItem>
-            <Button type="primary" onClick={addSubAssembly}>
+            <Button type="primary" onClick={addSubAssembly} className="mr-5">
               Add Sub Assembly
+            </Button>
+            <Button
+              onClick={() => toggleSpecifications(assembly.id, "assembly")}
+            >
+              Edit Specification
             </Button>
           </FormItem>
         </Form>
@@ -187,8 +345,16 @@ const AssemblyForm: React.FC = () => {
                     <Button
                       type="primary"
                       onClick={() => addPart(subAssembly.id)}
+                      className="mr-5"
                     >
                       Add Part
+                    </Button>
+                    <Button
+                      onClick={() =>
+                        toggleSpecifications(subAssembly.id, "subAssembly")
+                      }
+                    >
+                      Edit Specification
                     </Button>
                   </FormItem>
                 </Form>
@@ -211,12 +377,20 @@ const AssemblyForm: React.FC = () => {
                           </FormItem>
                           <FormItem>
                             <Button
+                              className="mr-5"
                               type="primary"
                               onClick={() =>
                                 addSubPart(subAssembly.id, part.id)
                               }
                             >
                               Add Sub Part
+                            </Button>
+                            <Button
+                              onClick={() =>
+                                toggleSpecifications(part.id, "part")
+                              }
+                            >
+                              Edit Specification
                             </Button>
                           </FormItem>
                         </Form>
@@ -240,6 +414,16 @@ const AssemblyForm: React.FC = () => {
                                       }
                                     />
                                   </FormItem>
+                                  <Button
+                                    onClick={() =>
+                                      toggleSpecifications(
+                                        subPart.id,
+                                        "subPart"
+                                      )
+                                    }
+                                  >
+                                    Edit Specification
+                                  </Button>
                                 </Form>
                               </Card>
                             </Col>
@@ -254,6 +438,10 @@ const AssemblyForm: React.FC = () => {
           ))}
         </Row>
       </Card>
+
+      <Button block className="mt-10" type="primary">
+        Create Product
+      </Button>
     </div>
   );
 };
